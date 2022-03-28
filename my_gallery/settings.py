b25ece6efe4f4f7f9f1e2dc71539a76f1916b2e0
Marial Clarie <marialclarie@gmail.com>
2022-03-28 19:10:05 +0300

    add allowedhost

--- my_gallery/settings.py
+++ my_gallery/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 
 # Application definition
