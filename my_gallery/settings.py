7179df1d97a1cc632c5a7f4ff455e03b4c4b6a53
Marial Clarie <marialclarie@gmail.com>
2022-03-28 21:13:49 +0300

    add pic to media

--- my_gallery/settings.py
+++ my_gallery/settings.py
@@ -119,13 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/images/'
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'static/images/')
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
